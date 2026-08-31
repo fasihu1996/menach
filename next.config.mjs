0c7eee7fe4bd7c39ac6e2eb6cd4493c9181c3457
fasihu1996 <fasih1996@gmail.com>
2026-08-31 22:33:56 +0200

    replace config with mjs version

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-    /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     experimental: {
         serverActions: {
             bodySizeLimit: "100mb",
